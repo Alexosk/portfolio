c39190ff2cfa89b51905c65db70fd99e4ba32cba
Alexosk <alexsigurdar@gmail.com>
2017-12-18 18:33:34 +0100

    Change menu layout

--- script/main.js
+++ script/main.js
@@ -1,5 +1,29 @@
-import SweetScroll from 'sweet-scroll';
+//countdown
+// Set the date we're counting down to
+var countDownDate = new Date("June 1, 2019 08:00:00").getTime();
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sweetScroll = new SweetScroll({ /* some options */ });
-}, false);
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 500);
