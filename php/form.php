699dc8dea5dc5a8a7fd44d3eb2040b327f608739
Alexosk <alexsigurdar@gmail.com>
2017-12-21 19:09:20 +0100

    Testing host and fix bugs

--- php/form.php
+++ php/form.php
@@ -1,12 +1,12 @@
 <?php
 require_once "Mail.php";
 
-$to = 'alexsigurdar@gmail.com';
+$to = 'alex@strangecousins.se';
 
-$host = "ssl://smtp.gmail.com";
+$host = "ssl://mailout.one.com";
 $port = "465";
-$username = 'alexsigurdar@gmail.com';
-$password = '4q2drYaVm6emp29Dd8EvtTR6ieeWPTf';
+$username = 'alex@strangecousins.se';
+$password = 'H62l2DuZZtu8KVGGN6kT';
 
 $name_field = $_POST['name'];
 $email_field = $_POST['email'];
@@ -14,7 +14,7 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 $body = "From: $name_field\n E-mail: $email_field\n Subject: $subject\n Message:\n $message";
 
-$headers = array ('From' => $email_field, 'To' => $to, 'Subject' => $subject);
+$headers = array ('From' => $to, 'To' => $to, 'Subject' => $subject);
 $smtp = Mail::factory('smtp',
 array ('host' => $host,
 'port' => $port,
@@ -30,18 +30,3 @@ echo($mail->getMessage());
 echo("Your e-mail was successfully sent!\n");
 }
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
