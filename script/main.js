c8325d371ab87812c2f799b5323eb82372ccb618
Alexosk <alexsigurdar@gmail.com>
2018-03-31 16:00:09 +0200

    Fix linter error on javascript

--- script/main.js
+++ script/main.js
@@ -1,29 +1,28 @@
-//countdown
+// countdown
 // Set the date we're counting down to
-var countDownDate = new Date("June 1, 2019 08:00:00").getTime();
+let countDownDate = new Date('June 1, 2019 08:00:00').getTime()
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+let x = setInterval(function () {
   // Get todays date and time
-  var now = new Date().getTime();
+  let now = new Date().getTime()
 
   // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+  let distance = countDownDate - now
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
   // Display the result in the element with id="demo"
-  document.getElementById("count-down").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById('count-down').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' +
+  seconds + 's '
 
   // If the count down is finished, write some text
   if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("count-down").innerHTML = "EXPIRED";
+    clearInterval(x)
+    document.getElementById('count-down').innerHTML = 'I am available for hiring'
   }
-});
+})
